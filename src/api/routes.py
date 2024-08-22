d442479a52cadbcc18fed6e02e2de2c5d5770bb1
Leivi521 <165715355+Leivi521@users.noreply.github.com>
2024-08-22 00:57:29 +0000

    todays update

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Caregiver, UserRequestCaregiver
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+
 
 api = Blueprint('api', __name__)
 
@@ -12,8 +14,108 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/user/signup', methods=['POST'])
+@jwt_required()
+def Users(request):
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    location= data.get('location')
+
+    if username and password: 
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user: 
+                return jsonify({'error': 'Username and password already exist'}), 400
+        else:
+             new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'Username and password successfully created'}), 200
+             
+    else: 
+         return jsonify({'error': 'Username and password are required'}), 400
+    
+    
+
+@api.route('/login/user', methods=['POST'])
+@jwt_required()
+def login (request):
+    print("login route hit")
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    if email is None or password is None:
+        return jsonify({"msg": "Please provide password and email"}), 400
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return jsonify({"msg": "This account does not exist"}), 404
+    if user.password != password: 
+        return jsonify({"msg": "The password provided does not match our records"}), 401
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify(access_token=access_token), 200
+
+@api.route('/login/cargiver', methods=['POST'])
+@jwt_required()
+def login (request):
+    print("login route hit")
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    if email is None or password is None:
+        return jsonify({"msg": "Please provide password and email"}), 400
+    user = Caregiver.query.filter_by(email=email).first()
+    if user is None:
+        return jsonify({"msg": "This account does not exist"}), 404
+    if user.password != password: 
+        return jsonify({"msg": "The password provided does not match our records"}), 401
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify(access_token=access_token), 200
+
+
+@api.route('/caregiver/signup', methods=['POST'])
+@jwt_required()
+def Users(request):
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    location= data.get('location')
+
+    if username and password: 
+        existing_user = Caregiver.query.filter_by(username=username).first()
+        if existing_user: 
+                return jsonify({'error': 'Username and password already exist'}), 400
+        else:
+             new_user = Caregiver(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'Username and password successfully created'}), 200
+             
+    else: 
+         return jsonify({'error': 'Username and password are required'}), 400
+
+
+@api.route('/caregiver/:id', methods=['GET'])
+def Profile(request):
+     
+
+    
+@api.route("/hello", methods=["GET"])
+@jwt_required()
+def get_hello():
+    
+    email = get_jwt_identity()
+    dictionary = {
+
+        "message": "hello world" + email
+    }
+    return jsonify(dictionary)
+       
+    
+
+    
+
+
+
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,81 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Carousel from "../component/carousel";
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+
+const Home = () => {
+    const { store, actions } = useContext(Context);
+
+    return (
+        <>
+            <header>
+				
+                <div className="container">
+                    <div className="header-logo">
+                        {/* Logo SVG goes here */}
+                        <h2>Caregiver Connect</h2>
+                    </div>
+                   
+                    <button className="cta-button-1">Sign-in</button>
+                    <button className="cta-button-1">Sign-up</button>
+                    
+                </div>
+            </header>
+
+            <div className="hero container">
+                <h1 className= "text-custom-1">Find a caregiver in 24 hours or less</h1>
+                {/* <input type="text" placeholder="Enter your email" /> */}
+                <button className="cta-button">Find a caregiver now!</button>
+            </div>
+
+            
+                <div className="section">
+                    <h2>Why Choose Us</h2>
+                    <p>Finding the right caregiver can take time, but we're here to help. Our team will work with you to find a caregiver who meets your needs, whether it's helping with daily tasks, providing companionship, or caring for a loved one. Here's why people love our service:</p>
+                </div>
+
+             
+                <div className="section-1">
+                    <div className="card">
+                     
+                        <h2 className="text-custom"><i class="fa-solid fa-user-group"></i>Fast Matching</h2>
+                        <p>We'll match you with a caregiver within 24 hours</p>
+                    </div>
+                    <div className="card">
+                    
+                        <h2 className="text-custom"><i class="fa-solid fa-heart"></i>Experienced Caregivers</h2>
+                        <p>All caregivers have at least 2 years of experience and have completed a background check</p>
+                    </div>
+                    <div className="card">
+                        
+                        <h2 className="text-custom"><i class="fa-solid fa-helmet-safety"></i>Safety First</h2>
+                        <p>We require all caregivers to be fully vaccinated for COVID-19</p>
+                    </div>
+                    <div className="card">
+                       
+                        <h2 className="text-custom"><i class="fa-solid fa-check"></i>Convenient</h2>
+                        <p>Find a caregiver who can work on your schedule, whether it's weekdays, weekends, or evenings</p>
+                    </div>
+                   
+                    
+                    
+                
+            </div>
+            <div>
+                    <Carousel />
+                    </div>
+            <footer>
+               
+            </footer>
+        </>
+    );
 };
+
+export default Home;
+
+
+
+
+
