8219fc885143ef8f13f3399857cb6c1d4761c422
Mariano Marcos <140490758+Zavanne@users.noreply.github.com>
2024-08-27 14:32:06 +0000

    updated caregiver portal name and initialized component for Patient Portal

---
+++ src/front/js/pages/CaregiverPortal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { FaCheck, FaTrashAlt } from 'react-icons/fa';
+import './CaregiverPortal.css'
+
+export const CaregiverPortal = () => {
+    return (
+        <div>
+            <h1>Welcome Caregiver!</h1>
+            <h3>Here are your current requests:</h3>
+            <div>
+               Requests
+            </div>
+            <h3>Here are your appointments for this week:</h3>
+            <div>
+                Appointments
+            </div>
+        </div>
+    )
+}
---
+++ src/front/js/pages/PatientPortal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { FaCheck, FaTrashAlt } from 'react-icons/fa';
+import './PatientPortal.css'
+
+export const PatientPortal = () => {
+    return (
+        <div>
+            <h1>Welcome Patient!</h1>
+            <h3>What do you want to do today?</h3>
+
+        </div>
+    )
+}
