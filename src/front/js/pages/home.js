07125b8f14a4877c79e043496cfad86fd0c11f37
Leivi521 <165715355+Leivi521@users.noreply.github.com>
2024-09-18 01:45:09 +0000

    merge completd

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -203,7 +203,7 @@ const Home = () => {
                     </div>
                 
                     </div>
-                    </div>
+                    
                     
                     <div>
                     
